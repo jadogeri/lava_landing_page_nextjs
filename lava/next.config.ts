31753a01d830a35a5f3f28cc816c58ac554a2268
joseph adogeri <josephadogeridev@gmail.com>
2026-05-12 21:40:48 -0500

    Updated next.config.ts

--- lava/next.config.ts
+++ lava/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+// Check if the current environment is production (building for GitHub Pages)
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: 'export',          
+  images: {
+    unoptimized: true,       
+  },
+  
+  // Only apply the subdirectory prefix when building for GitHub Pages production
+  basePath: isProd ? '/lava_landing_page_nextjs' : '',
 };
 
 export default nextConfig;
